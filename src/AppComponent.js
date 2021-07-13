0775eb8590d4cc47395a4a9aae0a09f0fb58226e
inod-noetic <inod.wagachchi@wearenoetic.com>
2021-07-13 21:17:50 +0530

    Design app component :art:

--- src/AppComponent.js
+++ src/AppComponent.js
@@ -1,29 +1,41 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-export default class extends React.Component {
-	render() {
-		return (
-            <div>
-				<div>
-        	        <span className="tag">Portlet Namespace:</span> 
-					<span className="value">{this.props.portletNamespace}</span>
-				</div>
-				<div>
-    	            <span className="tag">Context Path:</span>
-					<span className="value">{this.props.contextPath}</span>
-				</div>
-				<div>
-	                <span className="tag">Portlet Element Id:</span>
-					<span className="value">{this.props.portletElementId}</span>
-				</div>
-				
-				<div>
-					<span className="tag">Configuration:</span>
-					<span className="value pre">{JSON.stringify(this.props.configuration, null, 2)}</span>
-				</div>
-				
+const AppComponent = () => {
+	const imageData = [
+		{
+			title: "By The Water",
+			url:
+				"https://images.unsplash.com/photo-1558979158-65a1eaa08691?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80"
+		},
+		{
+			title: "A Winding Path",
+			url:
+				"https://images.unsplash.com/photo-1572276596237-5db2c3e16c5d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80"
+		},
+		{
+			title: "Look To The Ocean",
+			url:
+				"https://images.unsplash.com/photo-1507525428034-b723cf961d3e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1353&q=80"
+		},
+		{
+			title: "A Coastal City",
+			url:
+				"https://images.unsplash.com/photo-1551009175-8a68da93d5f9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1351&q=80"
+		},
+		{
+			title: "Rising Mountains",
+			url:
+				"https://images.unsplash.com/photo-1549880338-65ddcdfd017b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80"
+		}
+	]
+	return (
+		<div className="base">
+			<div className="base-container">
+				<h1>Hello World</h1>
 			</div>
-		);
-	}	
+		</div>
+	)
 }
+
+export default AppComponent
